f5281bb05e895bc9c0cd30a6dc78b3814cc9b1dc
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2025-08-11 17:23:42 +0600

    motion animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 import ShareButton from "@/components/ui/share-button";
+import FlipLink from "@/components/ui/text-effect-flipper";
 import WrapButton from "@/components/ui/wrap-button";
-import { Facebook, Instagram, Linkedin, Twitter } from "lucide-react";
+import { Facebook, Github, Instagram, Link, Linkedin, Twitter } from "lucide-react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -10,6 +12,23 @@ export default function Home() {
       <h1 className="text-4xl font-bold">Welcome to Face Around</h1>
       <WrapButton>Push More</WrapButton>
       <ShareButton className="mt-4" links={[{icon: Linkedin}, {icon: Facebook}, {icon: Twitter}, {icon: Instagram}]}>Social Links</ShareButton>
+      <FlipLink href="https://github.com/"> Github</FlipLink>
+
+      <motion.div 
+        className="flex items-center gap-1"
+        whileHover="hover"
+        initial="initial"
+      >
+        <motion.div
+          variants={{
+        initial: { color: "#fff" },
+        hover: { color: "#FE7500" }  // Twitter brand orange color
+          }}
+        >
+          <Twitter size={70} />
+        </motion.div>
+        <FlipLink href="https://twitter.com/"> Twitter</FlipLink>
+      </motion.div>
     </div>
   );
 }
